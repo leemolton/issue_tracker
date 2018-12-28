80eb22f461ca7f2ee1b1ad2871bb584e5b83ac4d
Lee Molton <leemolton@hotmail.co.uk>
2018-12-28 12:23:02 +0000

    Modified admin.py and forms.py for blog posts

--- posts/forms.py
+++ posts/forms.py
@@ -1,17 +1,16 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Post
-from .models import Entry
+from .models import Post, Entry
 
-class EntryForm(ModelForm):
+class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ('comment', )
-
+        fields = ('comment', 'user', 'body', )
+ 
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+        fields = ('title', 'content', 'image', 'tag', 'published_date',)
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

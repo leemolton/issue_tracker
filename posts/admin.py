bb21074f63943505e9f75aaaaaf345fd18ffc173
Lee Molton <leemolton@hotmail.co.uk>
2018-12-30 18:09:49 +0000

    Added into admin.py to display comments in admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Post, Entry
 
-admin.site.register(Post)
-admin.site.register(Entry)
+class PostAdmin(admin.ModelAdmin):
+   list_display = ('title', 'user', 'published_date')
+
+admin.site.register(Post, PostAdmin)
+
+
+class EntryAdmin(admin.ModelAdmin):
+   list_display = ('user', 'body', 'date_posted')
+   
+admin.site.register(Entry, EntryAdmin)
--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     A single Blog post
     """
     title = models.CharField(max_length=200)
+    user = models.CharField(max_length=200, null=True)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
@@ -13,18 +14,18 @@ class Post(models.Model):
     tag = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to="img", blank=True, null=True)
     
+    def __unicode__(self):
+        return self.title
+    
 
 class Entry(models.Model):
-    comment = models.CharField(max_length=500)
-    user = models.CharField(max_length=200)
+    comment = models.ForeignKey(Post, on_delete=None)
+    user = models.CharField(max_length=200, null=True)
     body = models.TextField(max_length=500, null=True, default=None)
     date_posted = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return 'Entry #{}'.format(self.id)
-    
-    def __unicode__(self):
-        return self.title
-    
+
     class Meta:
         verbose_name_plural = 'entries'   

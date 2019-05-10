c24f7bb974978d92b1c51746995139bb2e97bf71
Lee Molton <leemolton@hotmail.co.uk>
2019-05-10 17:43:04 +0000

    Updated Procfile and settings.py

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-release: python manage.py migrate --noinput
+release: python manage.py makemigrations
+release: python manage.py migrate
 web: gunicorn blog.wsgi:application
--- blog/settings.py
+++ blog/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 
-# if "DATABASE_URL" in os.environ:
-#     print("Using postgres!")
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-print("Postgres URL not found, using sqlite instead")
+if "DATABASE_URL" in os.environ:
+     print("Using postgres!")
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+     }
+else:
+    print("Postgres URL not found, using sqlite instead")
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.sqlite3',
